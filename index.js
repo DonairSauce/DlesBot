c99cbbb1f61970d4f60ea49c4bdd9ad3106a62f8
ShrekSauce <andre_pelland@hotmail.com>
2024-05-31 15:38:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const cron = require('node-cron');
-
+console.log(process.env.BOT_TOKEN);
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 client.once('ready', () => {
@@ -10,7 +10,6 @@ client.once('ready', () => {
     cron.schedule('0 5 * * *', () => {
         const guild = client.guilds.cache.get(process.env.GUILD_ID);
         const channel = guild.channels.cache.get(process.env.CHANNEL_ID);
-
         // Generate today's date string 
         const today = new Date();
         const dateString = today.toLocaleDateString('en-US', {year:'numeric', month:'long',day:'numeric'});
@@ -23,5 +22,5 @@ client.once('ready', () => {
         .catch(console.error);
     });
 });
-
+console.log(process.env.BOT_TOKEN);
 client.login(process.env.BOT_TOKEN);
